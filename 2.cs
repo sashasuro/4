b6de23617e4e9d6442accb19077d0345db6e38c1
sashasuro <123554714+sashasuro@users.noreply.github.com>
2023-11-06 10:42:12 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите координаты точки а (х)");
+            //double x1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки а (y)");
+            //double y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки b (х)");
+            //double x2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки b (y)");
+            //double y2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки c (х)");
+            //double x3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки c (y)");
+            //double y3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки d (x)");
+            //double x4 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите координаты точки d (y)");
+            //double y4 = Convert.ToInt32(Console.ReadLine());
+            //double d1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            //double d2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            //double d3 = Math.Sqrt(Math.Pow(x4 - x2, 2) + Math.Pow(y4 - y2, 2));
+            //double d4 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            //double p = d1 + d2 + d3 + d4;
+            //Console.WriteLine("Периметр ={p*f1}");
+            //double d = Math.Sqrt(Math.Pow(d1 + d4, 2));
+            //double p1 = d2 + d1 + d;
+            //double p2 = d3 + dl4 + d;
+            //Console.WriteLine($"Периметр 1 треуг. = {p1:f3},Периметр 2 треуг.= {p2:f3}\t");
+        }
+    }
+}
